ec67bd05b315e12bd0a121369c8a842833fc1863
suryaraj <suryaraj1971@gmail.com>
2014-07-27 15:07:44 +0530

    Solution to Programming Assignment 2 with Comments
    The comments explain the function that are defined as mentioned.
    The functions are properly formated.

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,41 @@
 # The function makeCacheMatrix takes a matrix as an argument and creates the matrix;
 # The function cacheSolve takes a matrix as an argument and computes its inverse and if it has already the inverse 
-  # then it returns the cache value without computing it again.
+## then it returns the cache value without computing it again.
 
-## This function initializes the object i as NULL, and defines four function namely, set, get, setInverse, getInverse
-   ### set function copies the matrix n, that it takes as an argument and initializes the  object i to NULL
-   ### get function returns the current matrix
-   ### setInverse function receives the inverse of the function and copies it to the object i
-   ### getInverse function returns the inverse of the current matrix 
+## This function initializes the object inverse as NULL, and defines four function namely, set, get, setInverse, getInverse
+### set function copies the matrix new_matrix, that it takes as an argument and initializes the  object inverse to NULL
+### get function returns the current matrix
+### setInverse function receives the inverse of the function and copies it to the object inverse
+### getInverse function returns the inverse of the current matrix 
 
-makeCacheMatrix <- function(m = matrix()) {
-  i <- NULL
-  set <- function(n){
-    m <<- n
-    i <<- NULL
+makeCacheMatrix <- function(current_matrix = matrix()) {
+  inverse <- NULL
+  set <- function(new_matrix){
+    current_matrix <<- new_matrix
+    inverse <<- NULL
   }
-  get <- function() m
-  setInverse <- function(inverse) i <<- inverse
-  getInverse <- function() i
+  get <- function() current_matrix
+  setInverse <- function(newinverse) inverse <<- newinverse
+  getInverse <- function() inverse
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## This function cache solve initializes the object i with the inverse value 
+## This function cache solve initializes the object inverse with the inverse value 
 ## and returns the inverse value of the current matrix
-   ### Computes the inverse of the current matrix
-   ### If the current matrix has the inverse(calculated early), then it returns that matrix from the cache displaying
-       #### the message "getting cached data".
+### Computes the inverse of the current matrix
+### If the current matrix has the inverse(calculated early), then it returns that matrix from the cache displaying
+#### the message "getting cached data".
 
 
-cacheSolve <- function(m, ...){
-  i <- m$getInverse()
-  if(!is.null(i)){
+cacheSolve <- function(current_matrix, ...){
+  inverse <- current_matrix$getInverse()
+  if(!is.null(inverse)){
     message("getting cached data")
-    return(i)
+    return(inverse)
   }
-  matrix <- m$get()
-  i <- solve(matrix, ...)
-  m$setInverse(i)
-  i
+  matrix <- current_matrix$get()
+  inverse <- solve(matrix, ...)
+  current_matrix$setInverse(inverse)
+  inverse
 }
